4bd1da127b495951d11b28943c23dad6c895bbd0
Ralph Moeritz <426109+rmoritz@users.noreply.github.com>
2020-05-30 18:39:50 +0200

    * Disable minimizing JS files in webpack config

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,14 +1,15 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
-// var nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
   entry: slsw.lib.entries,
-  // externals: [nodeExternals()],
   devtool: 'source-map',
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+  },
+  optimization: {
+    minimize: false
   },
   output: {
     libraryTarget: 'commonjs',
